48f526060d7aa8e15c6b6f3b92dee2924ba200af
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-11-22 13:44:23 +0000

    test

--- commulator/trans.py
+++ commulator/trans.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
 from googletrans import Translator
+
+text1 = "A Római Birodalom (latinul Imperium Romanum) az ókori Róma által létrehozott államalakulat volt a Földközi-tenger medencéjében"
+
+text2 = "Vysoké Tatry sú najvyššie pohorie na Slovensku a v Poľsku a sú zároveň jediným horstvom v týchto štátoch s alpským charakterom."
 
 translator = Translator()
 
-dt1 = translator.translate("Vysoké Tatry sú najvyššie pohorie na Slovensku a v Poľsku a sú zároveň jediným horstvom v týchto štátoch s alpským charakterom.", dest='en')
-print(dt1.text)
-
-dt2 = translator.translate("A Római Birodalom (latinul Imperium Romanum) az ókori Róma által létrehozott államalakulat volt a Földközi-tenger medencéjében", dest='yo')
-print(dt2.text)
+dt1 = translator.detect(text1)
+print(dt1)
+print('----------------------------------')
+dt2 = translator.detect(text2)
+print(dt2)
