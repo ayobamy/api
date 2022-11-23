b3bbe3ac9b5aee93ddc8175fee3e46db95ef15ba
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-11-23 20:24:10 +0000

    [update]: test

--- commulator/trans.py
+++ commulator/trans.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 from googletrans import Translator
 
-text = "The token is based on a seed which is updated once per hour and on the text that will be translated. Both are combined - by some strange math - in order to generate a final token (e.g. 744915.856682) which is used by the API to validate the request."
+text = """This Evaluation Quiz is to support your growth towards a software engineering role; we know how difficult the upcoming road will be, and we want to ensure you are ready for its challenges. The data has shown when students comprehend below a certain threshold they do not experience success after the program; this accountability is preventative to make sure we can help you get any help you may need. We are here to help!
+
+Once you have started the quiz, you may not click out, search on Google, open terminal, etc.
+
+Failure to stay within the exam window will reflect your final score.
+
+If you complete the quiz sooner than to be anticipated, that is acceptable. The quiz, however, cannot be paused once you have started.
+
+You are not allowed to ask peers for answers during the exam period. You can, however, use paper and pen to organize your thoughts.
+
+If you take the exam onsite during the specified exam time , you will receive 100% point potential.
+
+If you take the exam offsite during the specified time or at any time other than the specified exam time (onsite or offsite), you will receive 65% point potential."""
 
 translator = Translator(service_urls=[
-      'translate.google.com',
-      'translate.google.co.kr',
-    ])
+    'translate.google.com',
+    'translate.google.co.kr',
+])
 
-dt1 = translator.translate(text, dest='yo')
+dt1 = translator.translate(text, dest='fr')
 name = dt1.text
 print(name)
 # text = translator.detect(name)
