8bd94596d01fb3f26020459541c157ffb80ad9ba
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-12-08 23:55:31 +0000

    [update]: updated

--- commulator/webapp/commulator/views.py
+++ commulator/webapp/commulator/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "commulator/profile_list.html", {"profiles": profiles})
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "commulator/profile.html", {"profile": profile})
